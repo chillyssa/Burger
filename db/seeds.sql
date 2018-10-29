9afb680bb5245d079e3a5441e79b333b278f1d9c
Alyssa W <awilliams62090@gmail.com>
2018-10-29 08:52:17 -0600

    getting ready to deploy,  but bneeds better styling.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('All American', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Western Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Greek Turkey Burger', false);
